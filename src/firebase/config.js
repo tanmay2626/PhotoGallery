fc79abd36382fe26f9af7334202cda5869f91725
Tanmay Waykar <87329832+tanmay2626@users.noreply.github.com>
2022-02-05 17:35:16 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,7 @@ import { getStorage } from "firebase/storage"
 import { getFirestore, serverTimestamp, Timestamp } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCN7J__wcpvEU-BcxpP42iWaUiylqn5CzA",
-    authDomain: "tanmay-tamgram.firebaseapp.com",
-    projectId: "tanmay-tamgram",
-    storageBucket: "tanmay-tamgram.appspot.com",
-    messagingSenderId: "271038441846",
-    appId: "1:271038441846:web:3cca1be152c09a46e47257"
+  //Add Your credentials
   };
 
 const app = initializeApp(firebaseConfig);
